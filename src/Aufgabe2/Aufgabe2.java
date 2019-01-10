4133851afbe983ec739a644f3e593fb649c71eef
TheresaSchmotz <schmotz.theresa@gmail.com>
2019-01-10 01:11:42 +0100

    aufgabe 1

--- src/Aufgabe2/Aufgabe2.java
+++ src/Aufgabe2/Aufgabe2.java
@@ -1,5 +1,29 @@
 package Aufgabe2;
 
+import javax.swing.*;
+import java.util.Random;
+
 public class Aufgabe2 {
 
+    public static void main(String[] args) {
+
+        int arraylaenge = Integer.parseInt(JOptionPane.showInputDialog("Gib die Laenge des Arrays bitte ein"));
+        int[] array = new int[arraylaenge];
+
+        Random random = new Random();
+
+        for(int i = 0; i < size; i++)
+        {
+            Random random1 = new Random();
+
+            array[i] = random1.nextInt(1000);
+        }
+
+        for(int i = array.length-1; i >= 0; i--)
+        {
+            System.out.println(array[i]);
+        }
+
+
+    }
 }
